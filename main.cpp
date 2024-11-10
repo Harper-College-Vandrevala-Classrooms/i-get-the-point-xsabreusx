d59337e2a366a66c2b4a8ffd121af58b32b3c724
Humbert <122186863+xsabreusx@users.noreply.github.com>
2024-11-09 23:21:50 -0600

    functional operator overloads

--- main.cpp
+++ main.cpp
@@ -8,19 +8,65 @@ you get the point main lab cpp
 #include <cassert>
 using namespace std;
 
-class Points {
+class Point 
+{
 public:
+Point(double setx, double sety): x_coord(setx), y_coord(sety){};
+
+double getx(){
+    return x_coord;
+}
+double gety(){
+    return y_coord;
+}
+
+double operator-(const Point &pointA){
+return sqrt((pow(this->x_coord - pointA.x_coord, 2) + pow(this->y_coord - pointA.y_coord, 2)));
+}
+
+bool operator==(const Point &pointA){
+if(this->x_coord == pointA.x_coord && this->y_coord == pointA.y_coord){
+    return true;
+}
+else return false;
+}
+
+bool operator!=(const Point &pointA){
+if(this->x_coord != pointA.x_coord && this->y_coord != pointA.y_coord){
+    return true;
+}
+else return false;
+}
+
+Point operator/(const Point &pointA){
+return Point ((this->x_coord + pointA.x_coord)/2, (this->y_coord + pointA.y_coord)/2);
+}
 
 
 private:
-    vector<pair<double, double>> points; 
+
+double x_coord = 0;
+double y_coord = 0;
+
 };
 
 int main() {
+    /////////////// --- Test area --- /////////////
 
-    Points processor;
+    Point p1(1, 1);
+    Point p2(2, 2); 
 
+    double c1 = p1 - p2;
+    bool c2 = p1 == p2;
+    bool c3 = p1 != p2;
+    Point c4 = p1/p2;
 
+    cout << "\n Overload test for - operator Output should be " << sqrt(2) << ", output: " << c1;
+    cout << "\n Overload test for == operator Output should be " << false << ", output: " << c2;
+    cout << "\n Overload test for != operator Output should be " << true << ", output: " << c3;
+    cout << "\n Overload test for / operator Output should be " << "(1.5, 1.5), output: (" << c4.getx() << ", " << c4.gety() << ")";
+
+    ///////////////////////////////////////////////
 
     return 0;
 }
